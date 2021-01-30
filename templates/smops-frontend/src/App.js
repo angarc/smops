3b8261760cd581948b6e3f0ec1da77a4f7b67468
angarc <angarc37@gmail.com>
2021-01-29 21:15:31 -0700

    Added server code and react front end

---
+++ templates/smops-frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+import LoginScreen from './screens/LoginScreen/LoginScreen'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginScreen/>
+    </div>
+  );
+}
+
+export default App;
