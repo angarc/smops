7356a904eec1b35d67a25f58cf48b9de51e472e1
Victor Barros <victorcortezcb@gmail.com>
2021-01-30 17:41:57 -0300

    Created basic endpoints

--- server.py
+++ server.py
@@ -6,5 +6,30 @@ app = Flask(__name__)
 def index():
     return render_template('smops-frontend/build/index.html')
 
+@app.route('/create-user',methods=["POST"])
+def createUser():
+    request.form['username']
+    request.form['userpasswd']
+    request.form['userTwilioAPIKey']
+    request.form['SMSNumber']
+
+@app.route('/get-user-data',methods=["POST"])
+def getUserData():
+    request.form['username']
+    request.form['userpasswd']
+
+@app.route('/modify-user-data',methods=["POST"])
+def modifyUserData():
+    #FOR SECURITY REASONS
+    request.form['username']
+    request.form['userpasswd']
+    #Modifications
+    request.form['userTwilioAPIKey']
+    request.form['SMSNumber']
+
+@app.route('/get-machine-info',methods=["POST","GET"])
+def getMachineInfo():
+    return """{name:"nice machine"}"""
+
 if __name__ == '__main__':
     app.run()
